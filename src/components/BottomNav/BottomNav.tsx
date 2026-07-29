24bfe579551d4e066c447cf030e9e70e47e9f970
Candelaria Ferrari <ferrari.candelaria@gmail.com>
2026-07-29 17:48:17 -0300

    style: rediseñar el bottom nav sin botón flotante, con hover como en desktop

--- src/components/BottomNav/BottomNav.tsx
+++ src/components/BottomNav/BottomNav.tsx
@@ -10,37 +10,31 @@ interface BottomNavProps {
 function BottomNav({ onLogout, onNewTaskClick }: BottomNavProps) {
   return (
     <nav className="bottom-nav">
-      <div className="bottom-nav__links">
-        {NAV_ITEMS.map((item) => (
-          <NavLink
-            key={item.to}
-            to={item.to}
-            className={({ isActive }) =>
-              `bottom-nav__item${isActive ? " bottom-nav__item--active" : ""}`
-            }
-          >
-            {item.label}
-          </NavLink>
-        ))}
-      </div>
-
-      {/* Este bloque central solo reserva el espacio horizontal del botón +
-          (que se dibuja flotando arriba con position:absolute). Antes el +
-          se centraba con left:50% sobre TODO el nav, pero al tener solo 2
-          links + "Salir" repartidos con space-around, el centro del nav
-          coincidía con "Mis tareas" y el botón quedaba encima del texto. */}
-      <div className="bottom-nav__center">
-        <button
-          type="button"
-          className="bottom-nav__add-btn"
-          aria-label="Nueva tarea"
-          onClick={onNewTaskClick}
+      {NAV_ITEMS.map((item) => (
+        <NavLink
+          key={item.to}
+          to={item.to}
+          className={({ isActive }) =>
+            `bottom-nav__item${isActive ? " bottom-nav__item--active" : ""}`
+          }
         >
-          +
-        </button>
-      </div>
+          {item.label}
+        </NavLink>
+      ))}
 
-      <button type="button" className="bottom-nav__logout" onClick={onLogout}>
+      <button
+        type="button"
+        className="bottom-nav__item bottom-nav__item--new"
+        onClick={onNewTaskClick}
+      >
+        + Nueva tarea
+      </button>
+
+      <button
+        type="button"
+        className="bottom-nav__item bottom-nav__item--logout"
+        onClick={onLogout}
+      >
         Salir
       </button>
     </nav>
